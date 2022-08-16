318c3d784b21a53c3ed094ed91e3019b183b85bd
evelyn.lopez <evelyn.lopez@coppel.com>
2022-08-16 10:28:26 -0600

    se sube ejercicio 15-08-2022

---
+++ 15-08-2022/ejercicio.js
@@ -0,0 +1,50 @@
+class Maestro{
+    contructor(nombreMaestro,grupo,calificaciones){
+        this.nombreMaestro=nombreMaestro;
+        this.grupo=grupo;
+        this.calificaciones=[];
+    }
+    getNombreMaestro(){
+        return this.nombreMaestro;
+    }
+    getGrupo(){
+        return this.grupo;
+    }
+    setCalificaciones(alumno,calificacion){
+        this.alumno=alumno;
+        this.calificacion=calificacion;
+        this.calificaciones.push({"alumno":this.alumno, "calificacion":+this.calificacion});
+    }
+    getPromedio(){
+        let suma=0;
+        this.calificaciones.forEach((index) => {
+            suma= Number(suma) + Number(index.calificacion);
+        })
+        return suma/this.calificaciones.length;
+    }
+}
+
+class MaestroDeFisica extends Maestro{
+    constructor(nombre,grupo,antiguedad){
+        let s=super()
+        s.nombre=nombre;
+        s.grupo=grupo;
+        this.antiguedad=antiguedad;
+        this.calificaciones=[];       
+    }
+    getAntiguedad(){
+        return this.antiguedad;
+    }
+}
+class MaestroDeMusica extends Maestro{
+    constructor(nombre,grupo,edad){
+        let s=super()
+        s.nombre=nombre;
+        s.grupo=grupo;
+        this.edad=edad;
+        this.calificaciones=[];       
+    }
+    getEdad(){
+        return this.edad;
+    }
+}
